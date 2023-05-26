c9bfb39497817590731f42ae619bfa4a4960f679
pawan <pawans6343@gmail.com>
2023-05-27 01:05:09 +0530

    setup of db completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const { homePage } = require('../controllers/indexController');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', homePage);
 
 module.exports = router;
