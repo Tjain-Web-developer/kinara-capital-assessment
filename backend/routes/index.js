307b2f01348c92de14a1f008c42f6177ab8bb236
pawan <pawans6343@gmail.com>
2023-05-28 11:05:36 +0530

    completed inser,pagination,page-size routes and controllers

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
-const { homePage } = require('../controllers/indexController');
+const { homePage, createStudents, viewStudents, filterStudents } = require('../controllers/indexController');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', homePage);
 
+/* GET create student. */
+router.get("/create/students",createStudents);
+
+/* GET view students. */
+router.get("/view/students",viewStudents);
+
+/* GET filter students. */
+router.get("/filter/students",filterStudents);
 module.exports = router;
